bd182a494787fffb935cc21fc9e058d8903d3f8d
Marcin Kałamarski <marcin.kalamarski@chimeraprime.com>
2019-01-01 13:46:22 +0100

    Change ssh path

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ NODE_ENV=production parcel build index.js --public-url ./ --no-cache index.html
 
 cd dist/
 
-rsync -r -a -z -v -d --exclude=.DS_Store -e "ssh -i ~/.ssh/domowenauczanie" * mamalili@dm:/home/mamalili/domains/domowenauczanie.pl/public_html/generator
+rsync -r -a -z -v -d --exclude=.DS_Store -e "/usr/bin/ssh -i ~/.ssh/domowenauczanie" * mamalili@dm:/home/mamalili/domains/domowenauczanie.pl/public_html/generator
